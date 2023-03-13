3e90debaa3044b5af4d8f19be19284e627a74c06
Jonald Damaso <damasojonald@gmail.com>
2023-03-13 07:15:13 +0000

    Build: exercises1-5 done

---
+++ src/components/cat_card.tsx
@@ -0,0 +1,20 @@
+import Cat from '../data/cat'
+
+interface CatCardProps{
+    catObject: Cat;
+ }
+ const CatCard: React.FC<CatCardProps> = (props) => {
+    // console.log(
+	// 	'CatCard receiving props from App?! Come in App?! 😸 Props received are: ',
+	// 	props
+	// );  
+    return (
+        <div className='card'>
+            <h3 className='card__text card__header'>{props.catObject.name}</h3>
+            <p className='card__text'>Species: {props.catObject.species}</p>
+            <p className='card__text'>Favourite Food(s): {props.catObject.favFoods}</p>
+            <p className='card__text'>Birth Year: {props.catObject.birthYear}</p>
+        </div>
+    );
+ };
+export default CatCard;
